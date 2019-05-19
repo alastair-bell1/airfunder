fe43fe7b6cc5b08964b5865db02f3e8033560e18
alastair-bell1 <49374815+alastair-bell1@users.noreply.github.com>
2019-05-19 14:09:05 +0100

    fix base href

---
+++ precache-manifest.8eb4a11368dd7e2d265773ffb61954c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c05a315bfe5b40b8424cfdbbbeff5ab",
+    "url": "/airfunder/index.html"
+  },
+  {
+    "revision": "f0783f2e1942f3be0834",
+    "url": "/airfunder/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "199197b83e00d58ed84a",
+    "url": "/airfunder/static/css/main.c465803a.chunk.css"
+  },
+  {
+    "revision": "f0783f2e1942f3be0834",
+    "url": "/airfunder/static/js/2.b6964195.chunk.js"
+  },
+  {
+    "revision": "199197b83e00d58ed84a",
+    "url": "/airfunder/static/js/main.68afbbec.chunk.js"
+  },
+  {
+    "revision": "61290aab445756062e92",
+    "url": "/airfunder/static/js/runtime~main.555d0eaf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a78193dce439a41ee5496b533bc44af4.js"
+  "/airfunder/precache-manifest.8eb4a11368dd7e2d265773ffb61954c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/airfunder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
