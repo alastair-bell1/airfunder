8008e66438efa8f8a16a3cde899e542af1b69d70
alastair-bell1 <49374815+alastair-bell1@users.noreply.github.com>
2019-05-19 13:51:56 +0100

    first public mock up

---
+++ precache-manifest.a78193dce439a41ee5496b533bc44af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771eea21590f63e0422b349aebe92a76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81b6c8782145bc8a1708",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "af4895234ef7d4730282",
+    "url": "/static/css/main.c465803a.chunk.css"
+  },
+  {
+    "revision": "81b6c8782145bc8a1708",
+    "url": "/static/js/2.0062d2de.chunk.js"
+  },
+  {
+    "revision": "af4895234ef7d4730282",
+    "url": "/static/js/main.d5b17786.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
